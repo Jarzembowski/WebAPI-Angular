d608bea61eff3239bd91356f9933cc349c09916e
Jarzembowski <jargemboski@gmail.com>
2017-09-16 13:16:07 -0300

    first commit

---
+++ webAPI/webAPI/DAL/SubscriberContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using webAPI.Models;
+
+
+namespace webAPI.DAL
+{
+    public class SubscriberContext : DbContext
+    {        
+        public DbSet<Subscriber> Subscribers { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+
+}
---
+++ webAPI/webAPI/Models/Subscriber.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace webAPI.Models
+{
+    public class Subscriber
+    {
+        
+        public int Id { get; set; }
+
+        [StringLength(255)]
+        public string Email { get; set; }
+
+        public DateTime SubscribeDate { get; set; }
+    
+    }
+}
